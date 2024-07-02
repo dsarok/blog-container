52fdabca990e7bad10c1f2bcbe729ecfb13136ec
dsarok <48223277+dsarok@users.noreply.github.com>
2024-07-02 12:54:13 +0000

    added download feature

--- csv-downloader/app.js
+++ csv-downloader/app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+const fs = require('fs')
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
@@ -42,12 +43,36 @@ app.get("/about", function (req, res) {
 });
 
 app.get('/blogs', async function (req, res) {
-  const query = "Select heading, content from articles";
+  const query = "Select id, heading, content from articles";
   const data = await obj1.pool.query(query);
-  console.log('fetched data', data);
-  res.render('pages/blogs', { data: data[0] });
+  res.render('pages/blogs', { data: data });
 })
 
+app.get('/download/:id',async (req,res)=>{
+    const query = "Select * from articles where id = ?"
+    const id = req.params.id;
+    console.log('id is :',req.params.id)
+    res.setHeader('Content-Disposition', 'attachment; filename="largefile.csv"');
+    res.setHeader('Content-Type', 'text/plain');
+
+    const data = await obj1.pool.query(query,[id])
+    const titleKeys = Object.keys(data[0][0]);
+    const refinedData = [];
+    refinedData.push(titleKeys);
+    refinedData.push(Object.values(data[0][0]));
+
+    let csvContent="";
+    refinedData.forEach(row => {
+      csvContent += row.join(',') + '\n'
+    })
+    console.log(csvContent,'this is csv')
+    fs.writeFileSync('largefile.csv',csvContent,(err)=>{
+      console.log(err,'err is writing csv file')
+    })
+
+  const readStream = fs.createReadStream('./largefile.csv');
+  readStream.pipe(res);
+})
 
 app.listen(3000);
 console.log(new Date() + "restart 14 3000");
