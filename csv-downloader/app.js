e2a5bae3b378c7cb8b5c2bd6f0516bccbb212556
divya shankar anand <48223277+dsarok@users.noreply.github.com>
2024-07-24 00:27:10 +0000

    added for prometheus

--- csv-downloader/app.js
+++ csv-downloader/app.js
@@ -1,4 +1,6 @@
 var express = require("express");
+const client = require('prom-client')
+const register = new client.Registry()
 
 var app = express();
 var bodyParser = require("body-parser");
@@ -7,6 +9,12 @@ app.use(express.static('public'))
 const db = require('./service/service').database
 const obj1 = new db()
 obj1.createConnection();
+
+register.setDefaultLabels({
+  app: 'example-nodejs-app'
+})
+
+client.collectDefaultMetrics({ register })
 
 app.set("view engine", "ejs");
 
@@ -44,5 +52,11 @@ app.get('/download/:id', (req,res)=>{
  obj1.streamData(req.params.id,res)
 })
 
+app.get('/metrix',async (req,res)=>{
+  res.setHeader('Content-Type', register.contentType)
+  const value = await register.metrics();
+  console.log('metrix is this',value)
+  res.end(register.metrics().value)
+})
 app.listen(3000);
 console.log(new Date() + "restart 14 3000");
